8f12c4592b99554f44f10d7c21f6358ea6597565
takapon564 <ku.takahito058@gmail.com>
2018-11-09 06:33:52 +0900

    レイアウトの変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,18 @@ class BoardsController < ApplicationController
   def index
 
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    Board.create(board_params)
+  end
+
+  private 
+
+    def board_params
+      params.require(:board).permit(:name, :title, :body)
+    end
 end
