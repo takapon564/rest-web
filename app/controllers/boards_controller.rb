b8846d3e393d95484af3627099725059307d8296
takapon564 <ku.takahito058@gmail.com>
2018-11-15 06:30:26 +0900

    アクションの編集

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -23,7 +23,7 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @comment = @board.comments.new
+    @comment = Comment.new(board_id: @board.id)
     #binding.pry
   end
   
