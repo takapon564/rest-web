68c1ba3fba4fc01579f3b8768a6d71ba955a053a
takapon564 <ku.takahito058@gmail.com>
2018-11-18 10:21:10 +0900

    バグの修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -10,5 +10,5 @@
 
 class Tag < ApplicationRecord
   has_many :board_tag_relations, dependent: :delete_all
-  has_many :baords, through: :board_tag_relations
+  has_many :boards, through: :board_tag_relations
 end
